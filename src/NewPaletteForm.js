b756a1f3a58e0270d9e8011323954e0ca9e2b020
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-01-11 10:02:34 +0800

    added SavePalette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -88,6 +88,7 @@ class NewPaletteForm extends React.Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -124,6 +125,17 @@ class NewPaletteForm extends React.Component {
   handleChange(evt) {
     this.setState({ newName: evt.target.value });
   }
+
+  handleSubmit() {
+    let newName = "New Test Palette";
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLowerCase().replace(/ /g, "g"),
+      colors: this.state.colors,
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push("/");
+  }
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -132,6 +144,7 @@ class NewPaletteForm extends React.Component {
         <CssBaseline />
         <AppBar
           position="fixed"
+          color="default"
           className={clsx(classes.appBar, {
             [classes.appBarShift]: open,
           })}
@@ -149,6 +162,13 @@ class NewPaletteForm extends React.Component {
             <Typography variant="h6" noWrap>
               Persistent drawer
             </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer

d79c3f56d92687395e22d57bdb603d0220f3386a
ColonelBucket8 <freedomterran89@gmail.com>
2022-03-02 14:33:52 +0800

    added select material ui to select type of color to copy

--- src/components/palette/palette.component.jsx
+++ src/components/palette/palette.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setLevel } from "../../redux/palette/palette.slices";
+import { setLevel, setFormat } from "../../redux/palette/palette.slices";
 import Navbar from "../navbar/navbar.component";
 import ColorBox from "../color-box/color-box.component";
 import "./palette.style.css";
@@ -8,17 +8,27 @@ import "./palette.style.css";
 const Palette = ({ palette }) => {
   const dispatch = useDispatch();
   const level = useSelector((state) => state.palette.level);
+  const format = useSelector((state) => state.palette.format);
   const colors = palette.colors;
   const colorBox = colors[level].map((color) => (
-    <ColorBox key={color.name} background={color.hex} name={color.name} />
+    <ColorBox key={color.name} background={color[format]} name={color.name} />
   ));
 
   const changeLevel = (level) => {
     dispatch(setLevel(level));
   };
+
+  const changeFormat = (e) => {
+    dispatch(setFormat(e.target.value));
+  };
   return (
     <div className="Palette">
-      <Navbar level={level} changeLevel={changeLevel} />
+      <Navbar
+        format={format}
+        level={level}
+        changeLevel={changeLevel}
+        handleChange={changeFormat}
+      />
       <div className="Palette-colors">{colorBox}</div>
     </div>
   );
--- src/redux/palette/palette.slices.js
+++ src/redux/palette/palette.slices.js
@@ -6,15 +6,19 @@ export const paletteSlice = createSlice({
   initialState: {
     palettes: seedColors,
     level: 500,
+    format: "hex",
   },
 
   reducers: {
     setLevel: (state, action) => {
       state.level = action.payload;
     },
+    setFormat: (state, action) => {
+      state.format = action.payload;
+    },
   },
 });
 
-export const { setLevel } = paletteSlice.actions;
+export const { setLevel, setFormat } = paletteSlice.actions;
 
 export default paletteSlice.reducer;

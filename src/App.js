bf1d29bbeb203cb59da4b7f123a04cec24f162b0
ColonelBucket8 <freedomterran89@gmail.com>
2022-03-02 16:58:17 +0800

    Fixed palette route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Palette from "./components/palette/palette.component";
 import { generatePalette } from "./colorHelpers";
 
 const App = () => {
   const palettes = useSelector((state) => state.palette.palettes);
+  const findPalette = (id) => {
+    return palettes.find((palette) => palette.id === id);
+  };
   return (
-    <div>
-      <Palette palette={generatePalette(palettes[1])} />
-    </div>
+    // <div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette goes here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={(routeProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
+    // </div>
+    // <div>
+    //   <Palette palette={generatePalette(palettes[1])} />
+    // </div>
   );
 };
 
--- src/redux/palette/palette.slices.js
+++ src/redux/palette/palette.slices.js
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findPalette } from "./palette.utils";
 import seedColors from "../../seedColors";
 
 export const paletteSlice = createSlice({
   name: "palette",
   initialState: {
     palettes: seedColors,
+    palette: null,
     level: 500,
     format: "hex",
   },
@@ -16,9 +18,12 @@ export const paletteSlice = createSlice({
     setFormat: (state, action) => {
       state.format = action.payload;
     },
+    setPalette: (state, action) => {
+      state.palette = findPalette(state.palettes, action.payload);
+    },
   },
 });
 
-export const { setLevel, setFormat } = paletteSlice.actions;
+export const { setLevel, setFormat, setPalette } = paletteSlice.actions;
 
 export default paletteSlice.reducer;

a8991a2c713b39f72b7f34a0c2143ff789dfecf3
ColonelBucket8 <freedomterran89@gmail.com>
2022-03-03 15:26:40 +0800

    started styling homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,19 +2,56 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import MiniPalette from "../../components/mini-palette/mini-palette.component";
+import { withStyles } from "@material-ui/styles";
 
-const Homepage = () => {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+    color: "white",
+  },
+};
+
+const Homepage = (props) => {
+  const { classes } = props;
   const palettes = useSelector((state) => state.palette.palettes);
 
   return (
-    <div className="Homepage">
-      <MiniPalette />
-      <h1>React Color</h1>
-      {palettes.map((palette) => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Color</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette) => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Homepage;
+export default withStyles(styles)(Homepage);

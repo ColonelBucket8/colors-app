e859a653c83d94ea5378d93cdace5ff22b697a42
ColonelBucket8 <freedomterran89@gmail.com>
2022-03-03 15:10:11 +0800

    added miniPalette component and withstyles HOC

---
+++ src/components/mini-palette/mini-palette.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  main: {
+    backgroundColor: "purple",
+    border: "3px solid teal",
+  },
+  secondary: {
+    backgroundColor: "pink",
+  },
+};
+
+const MiniPalette = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.main}>
+      <h1>MINI PALETTE</h1>
+      <section className={classes.secondary}>asdfasdf</section>
+    </div>
+  );
+};
+
+export default withStyles(styles)(MiniPalette);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import MiniPalette from "../../components/mini-palette/mini-palette.component";
 
 const Homepage = () => {
   const palettes = useSelector((state) => state.palette.palettes);
 
   return (
     <div className="Homepage">
+      <MiniPalette />
       <h1>React Color</h1>
       {palettes.map((palette) => (
         <p>
